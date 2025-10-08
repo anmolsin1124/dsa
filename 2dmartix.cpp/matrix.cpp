e7809448cef0f90d4a6ea7fa41b0403f7d014358
anmolsin1124 <anmolsin1124@gmail.com>
2025-10-08 06:21:22 -0700

    my ifsrt vetor

--- 2dmartix.cpp/matrix.cpp
+++ 2dmartix.cpp/matrix.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 int main(){
+        vector<int> arr = {1,2,3,4,5,6,7,8,9,10};
+    // for(int i=0;i<10;i++){
+    for (int i = 0; i < arr.size()-1 ;i++)
+    {
+        cin>>arr[i];
+    }
+    for (int i = 0; i < arr.size()-1; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
     
     return 0;
 }
