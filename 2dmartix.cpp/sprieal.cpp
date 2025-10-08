5c186ab9e665470daeef91341c19ec4f991f74cd
anmolsin1124 <anmolsin1124@gmail.com>
2025-10-08 07:28:39 -0700

    popop sprieal

---
+++ 2dmartix.cpp/sprieal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int ar[6][6] = {1, 2, 3, 4, 5, 8, 6, 7, 8, 9, 10, 4, 11, 12, 13, 14, 15, 6, 16, 17, 18, 19, 20, 8, 21, 22, 23, 24, 25, 7};
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            cout << ar[i][j] << " ";
+        }
+    }
+
+
+    return 0;
+}
