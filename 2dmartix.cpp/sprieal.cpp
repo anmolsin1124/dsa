9d94cd3b25333cef7ac9a4188e9efa2e72add21d
anmolsin1124 <anmolsin1124@gmail.com>
2025-10-10 06:46:02 -0700

    3d arry

--- 2dmartix.cpp/sprieal.cpp
+++ 2dmartix.cpp/sprieal.cpp
@@ -4,18 +4,33 @@ using namespace std;
 int main()
 {
     int ar[6][6] = {1, 2, 3, 4, 5, 8, 6, 7, 8, 9, 10, 4, 11, 12, 13, 14, 15, 6, 16, 17, 18, 19, 20, 8, 21, 22, 23, 24, 25, 7};
-    for (int i = 0; i < 5; i++)
+    int top = 0, bottom = 5, left = 0, right = 5;
+    while (top <= bottom && left <= right)
     {
-        for (int j = 0; j < 6; j++)
+        for (int i = left; i <= right; i++)
         {
-        
-            cout<<ar[j][i]<< " ";
-            int name  = ar[j][i];
-            if(name % 2 == 0){
-                cout<<"even"<<endl;
-            
-
+            cout << ar[top][i] << " ";
         }
+        top++;
+        for (int i = top; i <= bottom; i++)
+        {
+            cout << ar[i][right] << " ";
+        }
+        right--;
+        
+            for (int i = right; i >= left; i--)
+            {
+                cout << ar[bottom][i] << " ";
+            }
+            bottom--;
+        
+        
+            for (int i = bottom; i >= top; i--)
+            {
+                cout << ar[i][left] << " ";
+            }
+            left++;
+        
     }
 
 
