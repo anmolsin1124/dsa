9a134118461953d8b7609c77a0a376a2813fcf46
anmolsin1124 <anmolsin1124@gmail.com>
2025-11-01 05:17:05 -0700

    reverse

---
+++ srting/reversestring.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str = "lomnahgnis";
+    int start = 0;
+    int end  = str.length()-1;
+    while (start<end)
+    {
+        swap(str[start],str[end]);
+        start++;
+        end--;
+
+    }
+    cout<<str<<endl;
+
+    
+
+    return 0;
+}
