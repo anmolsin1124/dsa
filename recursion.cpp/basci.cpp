1d4259d07fb462065028da7a6f2ebaab062aad2d
anmolsin1124 <anmolsin1124@gmail.com>
2025-11-04 05:53:24 -0800

    recursion.cpp/basci.cpp

--- recursion.cpp/basci.cpp
+++ recursion.cpp/basci.cpp
@@ -1,6 +1,83 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+// int print(int n)
+// {
+//     if (n == 0)
+//     {
+//         return 0;
+//     }
+//     print(n - 1);
+//     cout << n << endl;
+//     // print(n - 1);
+// }
+
+
+//////////////////////////////////////////////////power function
+// int power(int a, int b)
+// {
+//     if (b == 0)
+//     {
+//         return 1;
+//     }
+//     return a * power(a, b - 1);
+
+// }
+
+////////////////////////////////////////////sum of n natural numbers
+// int sum(int n)
+// {
+//     if (n == 0)
+//     {
+//         return 0;
+//     }
+//     return n + sum(n - 1);
+// }
+
+//////////////////////////////////////////////sum of spaures of n natural numbers
+// int sumsqure(int n)
+// {
+//     if (n == 0)
+//     {
+//         return 0;
+//     }
+//     return n * n + sumsqure(n - 1);
+// }
+/////////////////////////////////////////////////greatest common divisor
+// int gcd(int a, int b)
+// {
+//     if (b == 0)
+//     {
+//         return a;
+//     }
+//     return gcd(b, a % b);
+// }
+///////////////////////////////print arrry using fjuctio n or recuissuiob 
+// void printarray(int arr[], int index, int i)
+// {
+//     if (i == index)
+//     {
+//         return;
+//     }
+//     cout << arr[index] << " ";
+//     printarray(arr, index+1, i);
+// }
+////////////////////////////////sum of array function
+// int sumarray(int arr[], int index,int n)
+// {
+//     if (index == n)
+//     {
+//         return 0;
+//     }
+//     return arr[index] + sumarray(arr,index+1,n);
+// }
+
 int main(){
-    cout<<"Recursion basic file"<<endl;
+// {   power(2,9);
+
+// {   power(2,9);
+int arr[6]={1,2,3,4,5,6};
+int n=6;
+
+
     return 0;
 }
