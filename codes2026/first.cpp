2d520b0a4f93d54242d7981067b44ec7804dbbc8
ANMOL SINGH <anmolsin1124@.com>
2026-01-11 14:34:21 +0530

    hello  world

--- codes2026/first.cpp
+++ codes2026/first.cpp
@@ -4,5 +4,12 @@ int main()
 {
     cout << "hello  my name is anmol singh" << endl;
     cout << "my second commit" << endl;
+    int arr[10] = {1, 7, 9, 5, 2, 1, 4, 89, 45, 12};
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << " " << endl;
+    }
+
     return 0;
+    git config-- global user.email "you@example.com" git config-- global user.name "Your Name"
 }
